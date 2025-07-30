aa6959c316ebc6b33512b8321eb0f73f42024981
Yoon-Mac <sryoowk@gmail.com>
2025-07-30 20:53:45 +0900

    북 데이터 모델 수정

--- BookSearcherApp/BookSearcherApp/Model/BookData.swift
+++ BookSearcherApp/BookSearcherApp/Model/BookData.swift
@@ -13,14 +13,18 @@ struct RecentBookData: Hashable {
 
 struct SearchedBookData: Hashable {
     let title: String
-    let thumbnailURL: URL
     let author: String
+    let translator: String?
+    let thumbnailURL: URL
     let salePrice: String
+    let contents: String
 
     init(from dto: BookDTO) {
         title = dto.title
-        thumbnailURL = URL(string: dto.thumbnail) ?? URL(string: "https://placehold.co/120x174")!
         author = StringFormatter.formatList(dto.authors)
+        translator = StringFormatter.formatList(dto.translators)
+        thumbnailURL = URL(string: dto.thumbnail) ?? URL(string: "https://placehold.co/120x174")!
         salePrice = StringFormatter.formatPrice(dto.salePrice)
+        contents = dto.contents
     }
 }
