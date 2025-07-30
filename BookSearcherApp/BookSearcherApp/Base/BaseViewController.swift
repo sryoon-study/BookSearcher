a5958d356144c6b8ce82fd6cc0b5b7ee42e4afa4
Yoon-Mac <sryoowk@gmail.com>
2025-07-30 13:21:52 +0900

    BaseReactor, BaseViewController 수정

--- BookSearcherApp/BookSearcherApp/Base/BaseViewController.swift
+++ BookSearcherApp/BookSearcherApp/Base/BaseViewController.swift
@@ -8,19 +8,17 @@ import RxSwift
 class BaseViewController<R: Reactor>: UIViewController, View {
     // 모든 VC에 들어가는 것들
     var disposeBag = DisposeBag()
-    var reactor: R! // 매번 체크하지 않기 위해 주입후 무조건 있다고 가정
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        bind(reactor: reactor)
     }
 
     func setupUI() {
         // override에서 UI설정
     }
 
-    func bind(reactor _: R) {
+    func bind(reactor : R) {
         // 각 VC에서 상태 바인딩 시 override
     }
+    
 }
