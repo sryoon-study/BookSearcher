081e9146b184edf3c3e531eacf342b690e6c2f17
Yoon-Mac <sryoowk@gmail.com>
2025-07-30 20:54:15 +0900

    상세화면 리액터 / VC 구성

---
+++ BookSearcherApp/BookSearcherApp/Scenes/BookDetail/BookDetailViewController.swift
@@ -0,0 +1,152 @@
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class BookDetailViewController: BaseViewController<BookDetailReactor> {
+    
+    private let titleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        $0.textColor = .label
+    }
+    
+    private let authorLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 15)
+        $0.textColor = .secondaryLabel
+    }
+    
+    private let translatorLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12)
+        $0.textColor = .secondaryLabel
+    }
+    
+    private let thumbnailImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    private let priceLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        $0.textColor = .label
+    }
+    
+    private let contentsLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 13)
+        $0.textColor = .label
+        $0.numberOfLines = 0
+    }
+    
+    private let closeButton = UIButton(configuration: .filled()).then {
+        $0.configuration?.baseBackgroundColor = .systemGray6
+        $0.configuration?.title = "닫기"
+        $0.configuration?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+            outgoing.foregroundColor = UIColor.label
+            return outgoing
+        }
+    }
+    
+    private let favoriteButton = UIButton(configuration: .filled()).then {
+        $0.configuration?.baseForegroundColor = .systemBlue
+        $0.configuration?.title = "즐겨찾기 추가"
+        $0.configuration?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+            outgoing.foregroundColor = UIColor.systemBackground
+            return outgoing
+        }
+    }
+    
+    private let buttonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 20
+    }
+    
+    init(reactor: BookDetailReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(titleLabel)
+        view.addSubview(authorLabel)
+        view.addSubview(translatorLabel)
+        view.addSubview(thumbnailImageView)
+        view.addSubview(priceLabel)
+        view.addSubview(contentsLabel)
+        
+        buttonStackView.addArrangedSubview(closeButton)
+        buttonStackView.addArrangedSubview(favoriteButton)
+        view.addSubview(buttonStackView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(20)
+        }
+        authorLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+        }
+        translatorLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(authorLabel.snp.bottom).offset(10)
+        }
+        thumbnailImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(translatorLabel.snp.bottom).offset(10)
+        }
+        priceLabel.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(thumbnailImageView.snp.bottom).offset(10)
+        }
+        contentsLabel.snp.makeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        buttonStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(20)
+            $0.height.equalTo(50)
+        }
+        
+    }
+    
+    override func bind(reactor: BookDetailReactor) {
+        let book = reactor.currentState.book
+        
+        titleLabel.text = book.title
+        authorLabel.text = book.author
+        translatorLabel.text = book.translator
+        priceLabel.text = book.salePrice
+        contentsLabel.text = book.contents
+        thumbnailImageView.kf.setImage(with: book.thumbnailURL)
+
+        closeButton.rx.tap
+            .bind { [weak self] in
+                self?.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        favoriteButton.rx.tap
+            .bind {
+                print("책 담기 완료!") // 후속 작업으로 즐겨찾기 저장 처리 가능
+            }
+            .disposed(by: disposeBag)
+    }
+
+
+    
+}
---
+++ BookSearcherApp/BookSearcherApp/Scenes/BookDetail/Reactor/BookDetailReactor.swift
@@ -0,0 +1,33 @@
+
+import ReactorKit
+import RxSwift
+
+final class BookDetailReactor: BaseReactor<
+    BookDetailReactor.Action,
+    BookDetailReactor.Mutation,
+    BookDetailReactor.State
+> {
+    // 사용자 액션 정의 (사용자의 의도)
+    enum Action {}
+
+    // 상태변경 이벤트 정의 (상태를 어떻게 바꿀 것인가)
+    enum Mutation {}
+
+    // View의 상태 정의 (현재 View의 상태값)
+    struct State {
+        let book: SearchedBookData
+    }
+
+    // 생성자에서 초기 상태 설정
+    init(book: SearchedBookData) {
+        super.init(initialState: State(book: book))
+    }
+
+    // Action이 들어왔을 때 어떤 Mutation으로 바뀔지 정의
+    // 사용자 입력 → 상태 변화 신호로 변환
+    override func mutate(action: Action) -> Observable<Mutation> { .empty() }
+
+    // Mutation이 발생했을 때 상태(State)를 실제로 바꿈
+    // 상태 변화 신호 → 실제 상태 반영
+    override func reduce(state: State, mutation: Mutation) -> State { state }
+}
